22b5f38bcbd9a11596455f68844d907852dcf6d9
Samuel Jack <sam@seaturtlesoftware.com>
2013-07-25 09:29:43 +0100

    Added code

---
+++ RelayCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace SelectedItemsBindingDemo
+{
+    /// <summary>
+    /// Implements the ICommand interface
+    /// </summary>
+    /// <remarks>
+    /// Thanks to Josh Smith for this code: http://msdn.microsoft.com/en-us/magazine/dd419663.aspx
+    /// </remarks>
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        #endregion // ICommand Members
+    }
+
+}
---
+++ ViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Windows.Input;
+
+namespace SelectedItemsBindingDemo
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<string> _selectedNames = new ObservableCollection<string>();
+        string _summary;
+
+        public ViewModel()
+        {
+            _selectedNames.CollectionChanged += (sender, e) => UpdateSummary();
+        }
+
+        public IEnumerable<string> AvailableNames
+        {
+            get
+            {
+                return new string[] 
+                {
+                    "Abraham", "George", "James", "Joel", "John", "Peter", "Samuel", "Zachariah"
+                };
+            }
+        }
+
+        public ObservableCollection<string> SelectedNames
+        {
+            get
+            {
+                return _selectedNames;
+            }
+        }
+
+        public string Summary
+        {
+            get
+            {
+                return _summary;
+            }
+            private set
+            {
+                _summary = value;
+                OnPropertyChanged("Summary");
+            }
+        }
+
+        public ICommand SelectAll
+        {
+            get
+            {
+                return new RelayCommand(
+                    parameter =>
+                    {
+                        SelectedNames.Clear();
+                        foreach (var item in AvailableNames)
+                        {
+                            SelectedNames.Add(item);
+                        }
+                    });
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void UpdateSummary()
+        {
+            Summary = string.Format("{0} names are selected.", SelectedNames.Count);
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
